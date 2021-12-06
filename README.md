5f1d297298f13f833f0739d53763b08d9fc30584
Adam Laža <ad.laza32@gmail.com>
2021-12-06 14:28:44 +0100

    Update pipeline. (#1)
    Updated pipeline for lakes centerlines generation.
    Inspired by and based on @ache051 modifications with some minor modifications.

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 <img align="right" alt="Lake center lines for labels" width="250" src="lakelines.png" />
 
+An "updated version" of Lukas Martinelli's osm-lakelines using newer base images, Imposm3 0.11.1, Python 3.8, NetworkX 2+ and ungarj's updated label_centerlines repsitory. 
+There are two main reasons for this version, one being OpenMapTiles repository is still pulling from Luka's version 0.9 of global lake lines. 
+The second being the problems I had with foreign character sets (UTF-8) in Python 2 while running Luka's code.
+
+Database parameters are now parsed from .env and .env-postgres so to integrate with OpenMapTiles. The parameter -segmentize_maxlen is increased from 0.5 to 100 in order to tackle a ["Polygon has too few points" error](https://github.com/RanadeepPolavarapu/docker-nginx-http3/issues/7).
+
+Otherwise everything remains **relatively** identical.
+
 Calculate nice centered linestrings for labelling lake polygons.
 This repository provides downloadable Shapefiles containing center lines for lake polygons
 that you can use to label OSM lake polygons. You can match the OSM ID of the linestrings to your
 data source to merge the data.
 
 It is using [ungarj/label_centerlines](https://github.com/ungarj/label_centerlines) from Joachim Ungar to calculate the center lines. This doesn't always result the optimal result and has issues with more complicated lake structures but is sufficient enough for most use cases. Linestrings are not calculated for all lake polygons. Very small polygons, multi polygons and lakes without names are excluded.
-
-[**:open_file_folder: Download lake center lines as GeoJSON, Shapefile, GeoPackage from the releases page**](https://github.com/lukasmartinelli/osm-lakelines/releases/latest).
 
 ## Run Workflow
 
@@ -28,7 +34,7 @@ docker-compose up -d postgres
 Now download a OSM PBF extract and store it in the `./data` dir.
 
 ```bash
-wget -P ./data https://s3.amazonaws.com/metro-extracts.mapzen.com/zurich_switzerland.osm.pbf
+wget -P ./data https://download.geofabrik.de/europe/andorra-latest.osm.pbf
 ```
 
 Import the OSM PBF.
---
+++ src/calculate-centerlines/calculate_centerlines.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import datetime
+import fiona
+import multiprocessing
+from shapely.geometry import shape, mapping
+from functools import partial
+
+from label_centerlines import get_centerline
+
+def log(msg):
+    """Print provided log message in consistent format."""
+    print('{0}\t{1}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), msg))
+
+def main(args):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input_shp",
+        type=str,
+        help="input polygons"
+        )
+    parser.add_argument(
+        "output_geojson",
+        type=str,
+        help="output centerlines"
+        )
+    parser.add_argument(
+        "--segmentize_maxlen",
+        type=float,
+        help="maximum length used when segmentizing polygon borders",
+        default=100
+        )
+    parser.add_argument(
+        "--max_points",
+        type=int,
+        help="number of points per geometry allowed before simplifying",
+        default=3000
+        )
+    parser.add_argument(
+        "--simplification",
+        type=float,
+        help="value which increases simplification when necessary",
+        default=0.05
+        )
+    parser.add_argument(
+        "--smooth",
+        type=int,
+        help="smoothness of the output centerlines",
+        default=5
+        )
+    parser.add_argument(
+        "--output_driver",
+        type=str,
+        help="write to 'ESRI Shapefile' or 'GeoJSON' (default)",
+        default="GeoJSON"
+    )
+    parsed = parser.parse_args(args)
+    input_shp = parsed.input_shp
+    output_geojson = parsed.output_geojson
+    segmentize_maxlen = parsed.segmentize_maxlen
+    max_points = parsed.max_points
+    simplification = parsed.simplification
+    smooth_sigma = parsed.smooth
+    driver = parsed.output_driver
+
+    with fiona.open(input_shp, "r") as inp_polygons:
+        out_schema = inp_polygons.schema.copy()
+        out_schema['properties'].pop('NAME')
+        out_schema['geometry'] = "LineString"
+        with fiona.open(
+            output_geojson,
+            "w",
+            schema=out_schema,
+            crs=inp_polygons.crs,
+            driver=driver
+            ) as out_centerlines:
+            pool = multiprocessing.Pool(16)
+            func = partial(
+                worker,
+                segmentize_maxlen,
+                max_points,
+                simplification,
+                smooth_sigma
+            )
+            log("Start")
+            try:
+                feature_count = 0
+                for feature_name, output in pool.imap_unordered(
+                    func,
+                    inp_polygons
+                    ):
+                    feature_count += 1
+                    if output:
+                        #output["properties"]['NAME'] = output["properties"]['NAME']
+                        out_centerlines.write(output)
+                        log( "Written Feature " + str(feature_count) + ": " + feature_name)
+                        #print( "Written Feature %s: %s" %(
+                        #    feature_count,
+                        #    feature_name
+                        #    ))
+                    else:
+                        log("Invalid output for feature " + feature_name)
+                        #print("Invalid output for feature", feature_name)
+            except KeyboardInterrupt:
+                log("Caught KeyboardInterrupt, terminating workers")
+                #print("Caught KeyboardInterrupt, terminating workers")
+                pool.terminate()
+            except Exception as e:
+                #if "feature_name" in locals():
+                #    log(feature_name + ": FAILED " + str(e))
+                    #print ("%s: FAILED (%s)" %(feature_name, e))
+                #else:
+                log("feature: FAILED " + str(e))
+                    #print ("feature: FAILED (%s)" %(e))
+                #raise
+                pass
+            finally:
+                pool.close()
+                pool.join()
+
+    log("Done processing.")
+
+def worker(
+    segmentize_maxlen,
+    max_points,
+    simplification,
+    smooth_sigma,
+    feature
+    ):
+
+    geom = shape(feature['geometry'])
+    for name_field in ["name", "Name", "NAME"]:
+        if name_field in feature["properties"]:
+            feature_name = feature["properties"][name_field]
+            feature_id = feature["properties"]["OSM_ID"]
+            break
+        else:
+            feature_name = None
+    if feature_name:
+        log("Processing: " + feature_name + " with " + str(len(geom.exterior.coords)) + " nodes")
+        #print ("Processing: ", feature_name)
+
+    try:
+        centerlines_geom = get_centerline(
+            geom,
+            segmentize_maxlen=segmentize_maxlen,
+            max_points=max_points,
+            simplification=simplification,
+            smooth_sigma=smooth_sigma
+            )
+    except TypeError as e:
+        log(str(e))
+    except:
+        raise
+    if centerlines_geom:
+        feature['properties'].pop("NAME")
+        return (
+            feature_name,
+            {
+                'properties': feature['properties'],
+                'geometry': mapping(centerlines_geom)
+            }
+        )
+    else:
+        return (None, None)
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
